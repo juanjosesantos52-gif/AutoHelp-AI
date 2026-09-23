59716eb463f915227cc3ec41850e36a09192ae39
juanjosesantos52-gif <juanjose.santos52@gmail.com>
2026-09-22 20:35:27 -0400

    Refactor AppC.py to improve code structure

--- AppC.py
+++ AppC.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
